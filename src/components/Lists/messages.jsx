793a3e79e5ed94f72c2822403c218e918522e161
mcenberci <mcenberci@outlook.com>
2022-06-09 04:50:58 +0300

    second commit

--- src/components/Lists/messages.jsx
+++ src/components/Lists/messages.jsx
@@ -1,8 +1,6 @@
 const messages = [
-{sid:1,gid:0,message:"ProProPro"},
-{sid:1,gid:0,message:"ProProProooo"},
-{sid:0,gid:1,message:"ProProPro"},
-{sid:0,gid:1,message:"ProProPros"}
+{sid:1,gid:0,message:"Son kaça olur ?"},
+{sid:0,gid:1,message:"Malesef ürün fiyatında indirim yoktur."}
 ]
 if(JSON.parse(localStorage.getItem("messages"))===null) {localStorage.setItem("messages",JSON.stringify(messages));}
 export default messages;
